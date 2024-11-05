b02fd52981c4d4a27f14df3e974a76532fb1f3c4
Владислав Верига <verigavladislav@gmail.com>
2024-11-06 00:56:32 +0200

    first commit

---
+++ UnitTest7.1rec/UnitTest7.1rec.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest71rec
+{
+	TEST_CLASS(UnitTest71rec)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
